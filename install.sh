96a82c8d1128cfd547a22c22187e39fd612e031b
Kouhei Suzuki <kouhei.szk@gmail.com>
2013-06-13 00:14:23 +0900

    Keymap4Macの設定を追加

--- install.sh
+++ install.sh
@@ -1,29 +1,39 @@
 #!/bin/sh
 
-VIM_DIR=$HOME/.vim
-ECLIPSE_DIR=$HOME/.eclipse
+SHELL_DIR=$(cd $(dirname $0); pwd)
 
-if [ ! -d $VIM_DIR ]; then
-    mkdir -p $HOME/.vim/bundle
-    git clone https://github.com/Shougo/neobundle.vim $HOME/.vim/bundle/neobundle.vim
-    git clone https://github.com/Shougo/vimproc $HOME/.vim/bundle/vimproc
+VIM_DIR="$HOME/.vim"
+ECLIPSE_DIR="$HOME/.eclipse"
+
+if [ ! -d "$VIM_DIR" ]; then
+    mkdir -p "$HOME/.vim/bundle"
+    git clone https://github.com/Shougo/neobundle.vim "$HOME/.vim/bundle/neobundle.vim"
+    git clone https://github.com/Shougo/vimproc "$HOME/.vim/bundle/vimproc"
 fi
 
-if [ ! -d $ECLIPSE_DIR ]; then
-    mkdir -p $HOME/.eclipse
+if [ ! -d "$ECLIPSE_DIR" ]; then
+    mkdir -p "$ECLIPSE_DIR"
 fi
 
-ln -s .bash_profile $HOME
-ln -s .bashrc $HOME
-ln -s .eclipse/formatter $HOME/.eclipse
-ln -s .eclipse/keymap $HOME/.eclipse
-ln -s .gitconfig $HOME
-ln -s .gitignore_global $HOME
-ln -s .gosh_completions $HOME
-ln -s .profile $HOME
-ln -s .tmux.conf $HOME
-ln -s .vimrc $HOME
-ln -s .zshrc $HOME
-ln -s .zshrc_git $HOME
+ln -s "$SHELL_DIR/.bash_profile" "$HOME"
+ln -s "$SHELL_DIR/.bashrc" "$HOME"
+ln -s "$SHELL_DIR/.eclipse/formatter" "$ECLIPSE_DIR"
+ln -s "$SHELL_DIR/.eclipse/keymap" "$ECLIPSE_DIR"
+ln -s "$SHELL_DIR/.gitconfig" "$HOME"
+ln -s "$SHELL_DIR/.gitignore_global" "$HOME"
+ln -s "$SHELL_DIR/.gosh_completions" "$HOME"
+ln -s "$SHELL_DIR/.profile" "$HOME"
+ln -s "$SHELL_DIR/.tmux.conf" "$HOME"
+ln -s "$SHELL_DIR/.vimrc" "$HOME"
+ln -s "$SHELL_DIR/.zshrc" "$HOME"
+ln -s "$SHELL_DIR/.zshrc_git" "$HOME"
+
+KEYREMAP4MAC_DIR="$HOME/Library/Application Support/KeyRemap4MacBook"
+
+if [ -d "$KEYREMAP4MAC_DIR" ]; then
+    [ -f "$KEYREMAP4MAC_DIR/private.xml" ] && mv "$KEYREMAP4MAC_DIR/private.xml" "$KEYREMAP4MAC_DIR/private.xml.bk"
+            
+    ln -s "$SHELL_DIR/Library/Application Support/KeyRemap4MacBook/private.xml" "$KEYREMAP4MAC_DIR"
+fi
 
 echo 'Done...'
