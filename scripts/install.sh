1e7b493cd90801fb5840c3b47cdeed5077bb7956
Kouhei Suzuki <kouhei.szk@gmail.com>
2016-12-30 19:32:51 +0900

    スクリプトの権限を修正する

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,7 +6,7 @@ if [ $( uname ) == "Darwin" ]; then
   sh $SCRIPT_DIR/create_symbolick_links.sh
   sh $SCRIPT_DIR/install_command_line_tools.sh
   sh $SCRIPT_DIR/install_applications.sh
-  sh $SCRIPT_DIR/install_oh_my_zsh.sh
+  sh $SCRIPT_DIR/install_zsh_settings.sh
   sh $SCRIPT_DIR/install_anyenv.sh
 else
   echo "Nothing to do..."
