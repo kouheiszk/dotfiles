7fbae8d30316aa2247685cf276d39450d63031ea
Kouhei Suzuki <kouhei.szk@gmail.com>
2019-12-12 15:45:44 +0900

    Update

--- scripts/install_anyenv.sh
+++ scripts/install_anyenv.sh
@@ -1,56 +1,18 @@
 #!/bin/sh
 
 if [ ! -x "$(which anyenv)" ]; then
-  if [ ! -d "$HOME/.anyenv" ]; then
-    git clone https://github.com/anyenv/anyenv "$HOME/.anyenv"
-
-    case $SHELL in
-      "/bin/bash" ) source "$HOME/.bashrc" ;;
-      "/bin/zsh" ) source "$HOME/.zshrc" ;;
-    esac
-
-    mkdir -p $(anyenv root)/plugins
-    git clone https://github.com/znz/anyenv-update.git $(anyenv root)/plugins/anyenv-update
-  fi
-
-  mkdir -p $(anyenv root)/envs
-
-  # Install **env
-  anyenv install rbenv
-  anyenv install pyenv
-  anyenv install ndenv
-  anyenv install goenv
-  anyenv install phpenv
-
-  exec $SHELL -l
-fi
-
-if [ ! -d "$HOME/.local/bin" ]; then
-  mkdir -p $HOME/.local/bin
+  brew install anyenv
 fi
 
 if [ -x "$(which anyenv)" ]; then
-  # Install new versions at 2017.1.1
-  rbenv install 2.3.1
-  pyenv install 3.6.0
-  ndenv install v6.9.2
-  goenv install 1.7.4
-  # phpenv install 7.0.15
+  anyenv init
+  anyenv install --init
+
+  # Install new versions at 2019.12.12
+  rbenv install 2.6.4
+  nodenv install v13.3.0
 
   # update
   anyenv update
   anyenv versions
-fi
-
-# Install pyenv virtualenv
-if [ -d "$HOME/.anyenv/envs/pyenv/plugins/pyenv-virtualenv" ]; then
-  echo "Install pyenv-virtualenv"
-  git clone https://github.com/yyuu/pyenv-virtualenv.git "$HOME/.anyenv/envs/pyenv/plugins/pyenv-virtualenv"
-fi
-
-# Install composer
-if [ ! -f "$HOME/.local/bin/composer" ]; then
-  echo "Install composer"
-  curl -sS https://getcomposer.org/installer | php
-  mv composer.phar $HOME/.local/bin/composer
 fi
--- scripts/install_applications.sh
+++ scripts/install_applications.sh
@@ -20,73 +20,63 @@ brew doctor
 
 # Install from brew
 brew install \
-zsh \
-git \
-tig \
-hub \
-doxygen \
-chisel \
-ag \
-nkf \
-tree \
-tmux \
-reattach-to-user-namespace \
-mas \
-ansible \
-fswatch \
-jq \
-openssl \
-md5sha1sum \
-nmap \
-curl \
-wget \
-hugo \
-fzf \
-gawk \
-neovim/neovim/neovim \
-fswatch unison \
-yarn \
-terraform \
-laurent22/massren/massren
+  anyenv \
+  zsh \
+  git \
+  tig \
+  hub \
+  doxygen \
+  chisel \
+  ag \
+  nkf \
+  tree \
+  tmux \
+  reattach-to-user-namespace \
+  mas \
+  ansible \
+  fswatch \
+  jq \
+  openssl \
+  md5sha1sum \
+  nmap \
+  curl \
+  wget \
+  hugo \
+  fzf \
+  gawk \
+  neovim/neovim/neovim \
+  fswatch unison \
+  yarn \
+  terraform \
+  laurent22/massren/massren
 
 # Install from brew cask
 brew cask install \
-google-chrome \
-google-drive \
-firefox \
-google-japanese-ime \
-iterm2 \
-atom \
-docker \
-docker-toolbox \
-appcleaner \
-sequel-pro \
-dropbox \
-1password \
-shuttle \
-sketch \
-alfred \
-kindle \
-karabiner-elements
-# pg-commander
-# burn
-# virtualbox
+  google-chrome \
+  google-drive-file-stream \
+  firefox \
+  google-japanese-ime \
+  iterm2 \
+  atom \
+  docker \
+  docker-toolbox \
+  appcleaner \
+  sequel-pro \
+  dropbox \
+  1password \
+  sketch \
+  alfred \
+  kindle \
+  karabiner-elements
 
 # Remove outdated versions
-brew cleanup
-brew cask cleanup
+brew cleanup --prune
 
 # Install from mas
 # NOTE: mdfind "kMDItemAppStoreHasReceipt=1" | while read a;do echo $(mdls -name kMDItemAppStoreAdamID -raw "$a") $(echo $a|awk '{n=split($0, tmp, "/")}{print tmp[n]}');done | sort -k2 | awk '{ print "mas install " $1 " # " $0 }'
 mas install 406056744 # 406056744 Evernote.app
-mas install 682658836 # 682658836 GarageBand.app
 mas install 409183694 # 409183694 Keynote.app
 mas install 539883307 # 539883307 LINE.app
-mas install 409203825 # 409203825 Numbers.app
-mas install 409201541 # 409201541 Pages.app
-mas install 425955336 # 425955336 Skitch.app
 mas install 803453959 # 803453959 Slack.app
 mas install 425424353 # 425424353 The Unarchiver.app
-mas install 907364780 # 907364780 Tomato One.app
 mas install 497799835 # 497799835 Xcode.app
-mas install 408981434 # 408981434 iMovie.app
