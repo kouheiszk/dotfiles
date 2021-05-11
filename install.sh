e01567a1c2ffc5803e051b1d90a23792d79e7d01
Kouhei Suzuki <kouhei.szk@gmail.com>
2021-05-12 00:24:36 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,24 @@
 #!/bin/sh
 
-if command -v git &>/dev/null
-then
-    echo "git is already exists :)"
-else
-    echo "git is not exists :("
-    exit 1
-fi
+wget https://raw.githubusercontent.com/MikeMcQuaid/strap/master/bin/strap.sh -O /tmp/strap.sh
+bash /tmp/strap.sh
 
-if [ -d $HOME/.tmp/dotfiles ]; then
-  rm -rf $HOME/.tmp/dotfiles
-fi
-
-mkdir -p $HOME/.tmp
-git clone https://github.com/kouheiszk/dotfiles.git $HOME/.tmp/dotfiles
-cd $HOME/.tmp/dotfiles/
-
-make install
-
-cd -
+# if command -v git &>/dev/null
+# then
+#     echo "git is already exists :)"
+# else
+#     echo "git is not exists :("
+#     exit 1
+# fi
+#
+# if [ -d $HOME/.tmp/dotfiles ]; then
+#   rm -rf $HOME/.tmp/dotfiles
+# fi
+#
+# mkdir -p $HOME/.tmp
+# git clone https://github.com/kouheiszk/dotfiles.git $HOME/.tmp/dotfiles
+# cd $HOME/.tmp/dotfiles/
+#
+# make install
+#
+# cd -
